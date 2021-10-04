47bb21f17ba2e102fd15c7a28093bc38b73dd108
pragya644 <singhalpragya3@gmail.com>
2021-10-04 12:46:19 +0530

    Adding printing array reverse

--- 2darray.cpp
+++ 2darray.cpp
@@ -15,6 +15,13 @@ int main(){
         }
         cout<<endl;
     }
+     for(int i=0; i<n; i++)
+    {
+        for(int j=m-1; j>=0; j--){
+        cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 
 }
