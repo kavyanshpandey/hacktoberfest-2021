6625d7ac91955b0054523dc09abdc2e532ee194c
Pranay Kumar <65260061+pranay40@users.noreply.github.com>
2021-10-07 00:44:28 +0530

    Update 2darray.cpp

--- 2darray.cpp
+++ 2darray.cpp
@@ -18,10 +18,10 @@ int main(){
      for(int i=0; i<n; i++)
     {
         for(int j=m-1; j>=0; j--){
-        cout<<arr[i][j]<<" ";
+        cout<<arr[i][j]<<" "; //printing reverse
         }
-        cout<<endl;
-    }
+        
+    } 
     return 0;
 
 }
