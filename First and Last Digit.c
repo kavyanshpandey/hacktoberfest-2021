8f2a3bd611817d70e14ed7d62a2dede149dfff0a
geetuverma <92317942+geetuverma@users.noreply.github.com>
2021-10-11 20:55:37 +0530

    Update First and Last Digit.c
    sir please accept

--- First and Last Digit.c
+++ First and Last Digit.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 int main(void) 
 {
-	int N,T,i,LastNum,FirstNum;
+	int N,T,i,LN,FN;
 	scanf("%d",&T);
 	for(i=0;i<T;i++)
 	   {
 	       scanf("%d",&N);
-	       LastNum = N%10;
+	       LN = N%10;
 	       while(N!=0)
 	       {
 	         if(N<10)
 	           {
-	             FirstNum=N;
+	             FN=N;
 	           }
                N=N/10;
 	        }    
-	       printf("%d\n",FirstNum+LastNum);
+	       printf("%d\n",FN+LN);
 	   }
 	return 0;
 }
