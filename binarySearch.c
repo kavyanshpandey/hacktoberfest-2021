2d09c76ca787f1feed4dd793067db072e4dc826f
Gulshan kumar Jakhon <72978519+gulshanjakhon@users.noreply.github.com>
2021-10-13 12:08:05 +0530

    update binarysearch program

--- binarySearch.c
+++ binarySearch.c
@@ -1,20 +1,36 @@
-#include<stdio.h>
-int binarySearch(int A[],int low,int high,int target)
+// C Program to find element in array using binary search
+#include<stdio.h>           // Header file
+#include<conio.h>              // Header file
+int Bsearch(int , int []);              // function declare
+
+main()                                  // main function
 {
-   while(low<=high)
+int a[]={15,27,61,119,207,315,497};
+int n,i,res;
+printf("Enter no to be search : ");
+scanf("%d",&n);
+res = Bsearch(n,a);                     // call function
    {
-       int mid=(low+high)/2;
-       if(A[mid]<target)
-       {
-           low=mid+1;
-       }
-       else if(A[mid]>target)
-       {
-           high=mid-1;
-       }
-       else{
-           return mid;
-       }
-   }
-   return -1;
+    if(res==-1)
+        printf("Not found");
+    else
+        printf("Found at %d",res);
+    }
 }
+
+int Bsearch(int n, int a[])             // function define
+{
+int low=0,mid,high=10;
+while(low<=high)
+    {
+        mid=(low+high)/2;
+        if(a[mid]==n)
+            return mid+1;               //when found
+        else if(a[mid]>n)
+            high=mid-1;
+        else if(a[mid]<n)
+            low=mid+1;
+    }
+    return (-1);                         // not found
+}
+        // End of program
