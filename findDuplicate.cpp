b807f6439987127c7974cfe3a1e73122a217f5ed
Suryamani2002 <67437423+Suryamani2002@users.noreply.github.com>
2021-10-28 22:54:04 +0530

    To find dupliacte in the array
    Language used here is C++

---
+++ findDuplicate.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+uisng namespace std;
+
+int findDup(vector<int> nums){
+ 
+  unordered_map<int,int> map;
+  
+  for(int i=0;i<nums.size();i++){
+    if(map.find(nums[i])==map.end()){
+      map[nums[i]]=1;
+    }
+    else{
+      nums[i]++;
+    }
+    
+    for(auto &x:map){
+      if(x.second>1){
+        return x.first;
+      }
+    }
+  return -1;
+}
+int main()
+{
+  vector<int> num(10,20,30,20,40);
+  cout<<"Dupliacte number is "<<findDup(nums);
+  return 0;
+}
